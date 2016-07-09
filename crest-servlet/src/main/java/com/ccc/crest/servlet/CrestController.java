3928188ace09a505efc6da32dcc8abbe6c57b55a
Adams <chad.adams@us.panasonic.com>
2016-07-08 18:04:55 -0600

    start on event listeners

--- crest-servlet/src/main/java/com/ccc/crest/servlet/CrestController.java
+++ crest-servlet/src/main/java/com/ccc/crest/servlet/CrestController.java
@@ -22,40 +22,44 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.ccc.crest.cache.DataCache;
+import com.ccc.crest.servlet.auth.CrestClientInfo;
 import com.ccc.tools.ElapsedTimer;
 import com.ccc.tools.RequestThrottle;
 import com.ccc.tools.RequestThrottle.IntervalType;
 import com.ccc.tools.TabToLevel;
 import com.ccc.tools.servlet.CoreController;
+import com.ccc.tools.servlet.clientInfo.BaseClientInfo;
+import com.ccc.tools.servlet.events.AuthenticatedEventListener;
 
 @SuppressWarnings("javadoc")
-public class CrestController extends CoreController
+public class CrestController extends CoreController implements AuthenticatedEventListener
 {
-	public volatile DataCache dataCache;
-	
-	public Logger log;
-	
-	public CrestController()
-	{
-	    log = LoggerFactory.getLogger(getClass());
-	}
-	
+    public volatile DataCache dataCache;
+
+    public Logger log;
+
+    public CrestController()
+    {
+        log = LoggerFactory.getLogger(getClass());
+    }
+
     @Override
     public void init(Properties properties, TabToLevel format) throws Exception
     {
-    	super.init(properties, format);
-    	dataCache = new DataCache();
-//    	blockingExecutor.submit(new TestTask());
+        super.init(properties, format);
+        dataCache = new DataCache();
+        registerAuthenticatedListener(this);
+        // blockingExecutor.submit(new TestTask());
     }
-    
+
     @Override
     public void destroy()
     {
-    	if(dataCache != null)
-    		dataCache.clear();
-    	super.destroy();
+        if (dataCache != null)
+            dataCache.clear();
+        super.destroy();
     }
-    
+
     private class TestTask implements Callable<Void>
     {
         @Override
@@ -64,16 +68,16 @@ public class CrestController extends CoreController
             test4();
             return null;
         }
-        
+
         private void test4() throws InterruptedException
         {
             RequestThrottle rt = IntervalType.getRequestThrottle(1, 300);
             log.info("start here");
             ElapsedTimer.startTimer(0);
             ElapsedTimer.startTimer(1);
-            for(int i=0; i < 3; i++)
+            for (int i = 0; i < 3; i++)
             {
-                if(i >= 1)
+                if (i >= 1)
                     Thread.sleep(250);
                 rt.waitAsNeeded();
                 log.info(ElapsedTimer.getElapsedTime("waited: ", 1));
@@ -83,15 +87,16 @@ public class CrestController extends CoreController
             log.info(ElapsedTimer.getElapsedTime("total: ", 0));
             return;
         }
+
         private void test3() throws InterruptedException
         {
             RequestThrottle rt = new RequestThrottle(1, IntervalType.Minute);
             log.info("start here");
             ElapsedTimer.startTimer(0);
             ElapsedTimer.startTimer(1);
-            for(int i=0; i < 5; i++)
+            for (int i = 0; i < 5; i++)
             {
-                if(i >= 1)
+                if (i >= 1)
                     Thread.sleep(250);
                 rt.waitAsNeeded();
                 log.info(ElapsedTimer.getElapsedTime("waited: ", 1));
@@ -101,17 +106,18 @@ public class CrestController extends CoreController
             log.info(ElapsedTimer.getElapsedTime("total: ", 0));
             return;
         }
+
         private void test2() throws InterruptedException
         {
             RequestThrottle rt = new RequestThrottle(5, IntervalType.Second);
             log.info("start here");
             ElapsedTimer.startTimer(0);
             ElapsedTimer.startTimer(1);
-            for(int i=0; i < 30; i++)
+            for (int i = 0; i < 30; i++)
             {
-                if(i == 3)
+                if (i == 3)
                     Thread.sleep(1000);
-                if(i % 4 == 0)
+                if (i % 4 == 0)
                     Thread.sleep(250);
                 rt.waitAsNeeded();
                 log.info(ElapsedTimer.getElapsedTime("waited: ", 1));
@@ -121,15 +127,16 @@ public class CrestController extends CoreController
             log.info(ElapsedTimer.getElapsedTime("total: ", 0));
             return;
         }
+
         private void test1() throws InterruptedException
         {
             RequestThrottle rt = new RequestThrottle(1, IntervalType.Second);
             log.info("start here");
             ElapsedTimer.startTimer(0);
             ElapsedTimer.startTimer(1);
-            for(int i=0; i < 5; i++)
+            for (int i = 0; i < 5; i++)
             {
-                if(i >= 1)
+                if (i >= 1)
                     Thread.sleep(250);
                 rt.waitAsNeeded();
                 log.info(ElapsedTimer.getElapsedTime("waited: ", 1));
@@ -139,5 +146,26 @@ public class CrestController extends CoreController
             log.info(ElapsedTimer.getElapsedTime("total: ", 0));
             return;
         }
+    }
+
+    @Override
+    public void authenticated(BaseClientInfo clientInfo)
+    {
+        CrestClientInfo ccinfo = (CrestClientInfo)clientInfo; 
+        log.debug(ccinfo.getVerifyData().CharacterID + " " + ccinfo.getVerifyData().CharacterName + " authenticated");
+    }
+
+    @Override
+    public void refreshed(BaseClientInfo clientInfo)
+    {
+        CrestClientInfo ccinfo = (CrestClientInfo)clientInfo; 
+        log.debug(ccinfo.getVerifyData().CharacterID + " " + ccinfo.getVerifyData().CharacterName + " refreshed authentication");
+    }
+
+    @Override
+    public void dropped(BaseClientInfo clientInfo)
+    {
+        CrestClientInfo ccinfo = (CrestClientInfo)clientInfo; 
+        log.debug(ccinfo.getVerifyData().CharacterID + " " + ccinfo.getVerifyData().CharacterName + " dropped authentication");
     }
 }
