f49e257286d28de6cd08ecf90a25d3bbe39b54bd
Chad Adams <cadams@xmission.com>
2016-08-23 22:59:30 -0600

    put in LGPLv3 license/copyright

--- nee-d/src/main/java/com/ccc/crest/need/index/SidepanelPage.java
+++ nee-d/src/main/java/com/ccc/crest/need/index/SidepanelPage.java
@@ -1,3 +1,19 @@
+/*
+**  Copyright (c) 2016, Chad Adams.
+**
+**  This program is free software: you can redistribute it and/or modify
+**  it under the terms of the GNU Lesser General Public License as 
+**  published by the Free Software Foundation, either version 3 of the 
+**  License, or any later version.
+**
+**  This program is distributed in the hope that it will be useful,
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+**  GNU General Public License for more details.
+
+**  You should have received copies of the GNU GPLv3 and GNU LGPLv3
+**  licenses along with this program.  If not, see http://www.gnu.org/licenses
+*/
 package com.ccc.crest.need.index;
 
 import org.apache.wicket.Component;
@@ -6,7 +22,6 @@ import org.apache.wicket.authroles.authorization.strategies.role.annotations.Aut
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.protocol.http.WebSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
